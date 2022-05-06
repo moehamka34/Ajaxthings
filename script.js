fa3b02866ade97f670971ae1ad5b83e12171d5e8
Mohamed Hamka <mohamedhamka@Mohameds-MBP-2.attlocal.net>
2022-05-06 10:53:19 -0400

    finished the Ajax practice

--- script.js
+++ script.js
@@ -5,19 +5,34 @@
  // 1.  Jquery $.ajax() function
 // 2. the Browser's fetch() function
 // 3. Using the axios http clinet library
+//variables 
+const URL = "http://www.omdbapi.com/?apikey=a71b148a&t=Die+Hard";
 
-const URL ="http://www.omdbapi.com/?i=tt3896198&apikey=55fd9917"
+// element references 
+const $title = $('#title');
+const $year = $('#year');
+const $rating = $('#rating');
 
 
+//event listerners 
 
-$.ajax(URL).then(function(data) {
-    console.log('movie data is ready')
-    console.log(data)
-}, function(error) {
-    console.log('something is wrong')
-    console.log (error)
 
-})
+// functions
 
+function handleGetData(){
+
+    $.ajax(URL).then(function(data) {
+        console.log('movie data is ready')
+         console.log(data)
+        $title.text(data.Title)
+        $year.text(data.Year)
+        $rating.text(data.Rated)
+        $('main').append(`<img src="${data.Poster}"/>`)
+    }, function(error) {
+        console.log('something is wrong')
+        console.log(error)
+    })
+
+}
 
  
